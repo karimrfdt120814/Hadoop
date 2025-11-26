7fa08245a66685767ed68ad4f75f829586240368
Karimulla <karimrfdt120814@gmail.com>
2025-11-26 20:10:27 +0530

    spark practice

---
+++ src/main/java/controlSatements/conditionalStatements.java
@@ -0,0 +1,15 @@
+package controlSatements;
+
+public class conditionalStatements {
+
+    public static void main(String[] args){
+        int age = 15;
+
+        if(age>=18){
+            System.out.println("The Person is eligible for Vote");
+        }
+        else{
+            System.out.println("The person not eligible for Vote");
+        }
+    }
+}
---
+++ src/main/java/controlSatements/ifElseif.java
@@ -0,0 +1,13 @@
+package controlSatements;
+
+public class ifElseif {
+
+    public static void main(String[] args){
+        int a = 10;
+        if(a==10){
+            System.out.println("value of a is:10");
+
+        }
+
+    }
+}
