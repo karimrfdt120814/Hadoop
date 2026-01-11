77b3995c311f2788389b2b5e88bd77c4eba6b99e
Karimulla <karimrfdt120814@gmail.com>
2026-01-11 20:07:29 +0530

    java practice

---
+++ src/main/java/javaPrac/sortArrayDESC.java
@@ -0,0 +1,14 @@
+package javaPrac;
+
+public class sortArrayDESC {
+    public static void main(String[] args) {
+        int a[] = {1,4,2,5,6,3,9,25,14};
+
+        for(int i=a.length;i<a.length;i++){
+            if(a[i] < a[i+1]){
+                int temp = a[i];
+
+            }
+        }
+    }
+}
--- src/main/scala/clousres.scala
+++ src/main/scala/clousres.scala
@@ -1,34 +1,89 @@
 object clousres {
 
-  def main(args:Array[String]):Unit={
+  def main(args: Array[String]): Unit = {
 
-    /****
+    /** **
      * scala closure is a function whose return value
      * depends on the one or more variables declared out side of the function...
      */
 
-//    val factor = 10
-//
-//    val multiplication = (x:Int)=>x*factor
-//
-//    println(multiplication(5)) //prints 50
-//
-//
-//
-//val first_name = "Karimulla"
-//
-//    val full_name = (x:String) => x + " " + first_name
-//
-//    println("The full Name is:" +full_name("Shaik"))
+    //example1:
+
+    val factor = 10
+
+        val multiplication = (x:Int)=>x*factor
+
+        println(multiplication(5)) //prints 50
 
 
-    val number = 100
+//example2:
 
-    val sum = (x:Int,y:Int) => x+y
+    val first_name = "Karimulla"
 
-    val res= sum(1,4)
-    println(res)
+        val full_name = (x:String) => x + " " + first_name
+
+        println("The full Name is:" +full_name("Shaik"))
+
+
+      //example3:
+
+
+        val number = 100
+
+        val sum = (x:Int) => x+number
+        sum(2)
+        println(sum(2))
+
+
+//example4:
+
+    def counterFunction(): () => Int = {
+      var count = 0
+      () => {
+        count += 1
+        count
+      }
+    }
+
+    val countNow = counterFunction()
+
+    println(countNow())
+    println(countNow())
+    println(countNow())
+
+      /**
+      ****example 5
+       */
+
+    def makeLogger(prefix:String):String=>Unit={
+      (msg:String)=>println(s"[$prefix  $msg")
+    }
+
+    val errorLog = makeLogger("Error: ")
+    val infoLog = makeLogger("Info: ")
+
+    println(errorLog("Something went wrong.... please check the flow"))
+    println(infoLog("Application has started...."))
+
+    /**
+     * example:6
+     */
+
+    def bankAccount(InitialBalance:Double):(Double=>Unit,Double=>Unit, ()=>Double)={
+      var balance = InitialBalance
+      val deposit = (amount: Double) => balance += amount
+      val withDraw=(amount:Double)=> balance -=amount
+      val getBalance=()=> balance
+
+      (deposit, withDraw,getBalance)
+    }
+
+    val (deposit,withDraw,balance)=bankAccount(100)
+    deposit(50)
+    withDraw(20)
+    println("remaining balance is: " +balance())
 
   }
+
 
 }
